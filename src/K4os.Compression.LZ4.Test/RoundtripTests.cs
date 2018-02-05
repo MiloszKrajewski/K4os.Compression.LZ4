784ebcbba2e1a6c0491ea35ed82bdd9e96f9f463
Milosz Krajewski <milosz.krajewski@getbusy.com>
2018-02-05 17:47:32 +0000

    unit tests

--- src/K4os.Compression.LZ4.Test/RoundtripTests.cs
+++ src/K4os.Compression.LZ4.Test/RoundtripTests.cs
@@ -1,108 +1,108 @@
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using LZ4;
-//using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LZ4;
+using Xunit;
 
-//namespace K4os.Compression.LZ4.Test
-//{
+namespace K4os.Compression.LZ4.Test
+{
 
-//	public class RoundtripTests
-//	{
-//		const string Lorem =
-//			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-//			"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
-//			"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
-//			"nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
-//			"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
-//			"Excepteur sint occaecat cupidatat non proident, " +
-//			"sunt in culpa qui officia deserunt mollit anim id est laborum.";
+	public class RoundtripTests
+	{
+		const string Lorem =
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+			"sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+			"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+			"nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+			"reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+			"Excepteur sint occaecat cupidatat non proident, " +
+			"sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-//		private static byte[] Collect(IEnumerable<byte[]> chunks)
-//		{
-//			var arrays = chunks.ToArray();
-//			var length = arrays.Select(x => x.Length).Sum();
-//			var result = new byte[length];
-//			var index = 0;
-//			foreach (var c in arrays)
-//			{
-//				Array.Copy(c, 0, result, index, c.Length);
-//				index += c.Length;
-//			}
+		private static byte[] Collect(IEnumerable<byte[]> chunks)
+		{
+			var arrays = chunks.ToArray();
+			var length = arrays.Select(x => x.Length).Sum();
+			var result = new byte[length];
+			var index = 0;
+			foreach (var c in arrays)
+			{
+				Array.Copy(c, 0, result, index, c.Length);
+				index += c.Length;
+			}
 
-//			return result;
-//		}
+			return result;
+		}
 
-//		private static void Roundtrip(byte[] source)
-//		{
-//			var compressedOld = LZ4Codec.Encode(source, 0, source.Length);
-//			var compressedNew = LZ4Interface.Encode(source);
+		private static void Roundtrip(byte[] source)
+		{
+			var compressedOld = LZ4Codec.Encode(source, 0, source.Length);
+			var compressedNew = LZ4Interface.Encode(source);
 
-//			Assert.Equal(
-//				source,
-//				LZ4Codec.Decode(compressedNew, 0, compressedNew.Length, source.Length));
+			Assert.Equal(
+				source,
+				LZ4Codec.Decode(compressedNew, 0, compressedNew.Length, source.Length));
 
-//			Assert.Equal(
-//				source,
-//				LZ4Interface.Decode(compressedNew, source.Length));
+			Assert.Equal(
+				source,
+				LZ4Interface.Decode(compressedNew, source.Length));
 
-//			Assert.Equal(
-//				source,
-//				LZ4Interface.Decode(compressedOld, source.Length));
-//		}
+			Assert.Equal(
+				source,
+				LZ4Interface.Decode(compressedOld, source.Length));
+		}
 
-//		[Theory]
-//		[InlineData(160)]
-//		[InlineData(0)]
-//		[InlineData(255)]
-//		[InlineData(65)]
-//		public void SingleByteRountrip(byte value)
-//		{
-//			Roundtrip(new[] { value });
-//		}
+		[Theory]
+		[InlineData(160)]
+		[InlineData(0)]
+		[InlineData(255)]
+		[InlineData(65)]
+		public void SingleByteRountrip(byte value)
+		{
+			Roundtrip(new[] { value });
+		}
 
-//		[Theory]
-//		[InlineData(160, 33)]
-//		[InlineData(0, 13)]
-//		[InlineData(0, 15)]
-//		[InlineData(0, 17)]
-//		[InlineData(255, 1000)]
-//		[InlineData(65, 67)]
-//		public void RepeatedByteRountrip(byte value, int length)
-//		{
-//			var bytes = new byte[length];
-//			for (var i = 0; i < length; i++) bytes[i] = value;
+		[Theory]
+		[InlineData(160, 33)]
+		[InlineData(0, 13)]
+		[InlineData(0, 15)]
+		[InlineData(0, 17)]
+		[InlineData(255, 1000)]
+		[InlineData(65, 67)]
+		public void RepeatedByteRountrip(byte value, int length)
+		{
+			var bytes = new byte[length];
+			for (var i = 0; i < length; i++) bytes[i] = value;
 
-//			Roundtrip(bytes);
-//		}
+			Roundtrip(bytes);
+		}
 
-//		[Theory]
-//		[InlineData(1)]
-//		[InlineData(1000)]
-//		[InlineData(0x7FFF)]
-//		[InlineData(0xFFFF)]
-//		[InlineData(0x123456)]
-//		public void RepeatLoremIpsum(int length)
-//		{
-//			var loremBytes = Encoding.UTF8.GetBytes(Lorem);
-//			var buffer = Collect(Enumerable.Repeat(loremBytes, length / Lorem.Length + 1));
+		[Theory]
+		[InlineData(1)]
+		[InlineData(1000)]
+		[InlineData(0x7FFF)]
+		[InlineData(0xFFFF)]
+		[InlineData(0x123456)]
+		public void RepeatLoremIpsum(int length)
+		{
+			var loremBytes = Encoding.UTF8.GetBytes(Lorem);
+			var buffer = Collect(Enumerable.Repeat(loremBytes, length / Lorem.Length + 1));
 
-//			Roundtrip(buffer);
-//		}
+			Roundtrip(buffer);
+		}
 
-//		[Theory]
-//		[InlineData(0, 1000)]
-//		[InlineData(1, 0x7FFF)]
-//		[InlineData(2, 0xFFFF)]
-//		[InlineData(3, 0xFFFF)]
-//		[InlineData(4, 0x123456)]
-//		public void UncompressibleData(int seed, int length)
-//		{
-//			var buffer = new byte[length];
-//			new Random(seed).NextBytes(buffer);
+		[Theory]
+		[InlineData(0, 1000)]
+		[InlineData(1, 0x7FFF)]
+		[InlineData(2, 0xFFFF)]
+		[InlineData(3, 0xFFFF)]
+		[InlineData(4, 0x123456)]
+		public void UncompressibleData(int seed, int length)
+		{
+			var buffer = new byte[length];
+			new Random(seed).NextBytes(buffer);
 
-//			Roundtrip(buffer);
-//		}
-//	}
-//}
+			Roundtrip(buffer);
+		}
+	}
+}
---
+++ src/K4os.Compression.LZ4.Test/Tools.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace K4os.Compression.LZ4.Test
+{
+	public class Tools
+	{
+		public static uint Adler32(byte[] data)
+		{
+			const uint modAdler = 65521;
+
+			uint a = 1, b = 0;
+			var len = data.Length;
+
+			for (var index = 0; index < len; ++index)
+			{
+				a = (a + data[index]) % modAdler;
+				b = (b + a) % modAdler;
+			}
+
+			return (b << 16) | a;
+		}
+
+		public static byte[] LoadChunk(string filename, int index, int length)
+		{
+			using (var file = File.OpenRead(filename))
+			{
+				length = length < 0 ? (int) (file.Length - index) : length;
+				var src = new byte[length];
+				file.Seek(index, SeekOrigin.Begin);
+				file.Read(src, 0, length);
+				return src;
+			}
+		}
+
+		public static string FindFile(string filename) =>
+			Path.Combine(FindRoot(), filename);
+
+		private static string FindRoot(string path = ".")
+		{
+			bool IsRoot(string p) =>
+				Path.GetFullPath(p) == Path.GetFullPath(Path.Combine(p, ".."));
+
+			while (true)
+			{
+				if (Directory.Exists(Path.Combine(path, "./.git"))) return path;
+				if (IsRoot(path)) return null;
+
+				path = Path.Combine(path, "..");
+			}
+		}
+
+		public static void SameBytes(byte[] source, byte[] target)
+		{
+			if (source.Length != target.Length)
+				throw new ArgumentException($"Arrays are not same length: {source.Length} vs {target.Length}");
+
+			var length = source.Length;
+			for (var i = 0; i < length; i++)
+			{
+				if (source[i] != target[i])
+					throw new ArgumentException($"Arrays differ at index {i}: {source[i]} vs {target[i]}");
+			}
+		}
+
+		public static void SameBytes(byte[] source, byte[] target, int length)
+		{
+			if (source.Length < length)
+				throw new ArgumentException($"Source array is too small: {source.Length}");
+			if (target.Length < length)
+				throw new ArgumentException($"Target array is too small: {target.Length}");
+
+			for (var i = 0; i < length; i++)
+			{
+				if (source[i] != target[i])
+					throw new ArgumentException($"Arrays differ at index {i}: {source[i]} vs {target[i]}");
+			}
+		}
+	}
+}
