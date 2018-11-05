8116e610dd67fa01a08ae150fce476e7605b6b0b
Milosz Krajewski <milosz.krajewski@getbusy.com>
2018-11-05 10:54:36 +0000

    removed accidently added file with secrets (damn!)

--- src/K4os.Compression.LZ4/LZ4Level.cs
+++ src/K4os.Compression.LZ4/LZ4Level.cs
@@ -7,34 +7,34 @@ namespace K4os.Compression.LZ4
 	{
 		/// <summary>Fast compression.</summary>
 		L00_FAST = 0,
-		
+
 		/// <summary>High compression, level 3.</summary>
 		L03_HC = 3,
-		
+
 		/// <summary>High compression, level 4.</summary>
 		L04_HC = 4,
-		
+
 		/// <summary>High compression, level 5.</summary>
 		L05_HC = 5,
-		
+
 		/// <summary>High compression, level 6.</summary>
 		L06_HC = 6,
-		
+
 		/// <summary>High compression, level 7.</summary>
 		L07_HC = 7,
-		
+
 		/// <summary>High compression, level 8.</summary>
 		L08_HC = 8,
-		
+
 		/// <summary>High compression, level 9.</summary>
 		L09_HC = 9,
-		
+
 		/// <summary>Optimal compression, level 10.</summary>
 		L10_OPT = 10,
-		
+
 		/// <summary>Optimal compression, level 11.</summary>
 		L11_OPT = 11,
-		
+
 		/// <summary>Maximum compression, level 12.</summary>
 		L12_MAX = 12,
 	}
