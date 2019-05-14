08bc53e870e2f42dfc2a483c89434ea67d477e8a
Milosz Krajewski <milosz.krajewski@getbusy.com>
2019-05-14 11:03:02 +0100

    failing test added

--- src/K4os.Compression.LZ4.Streams.Test/RoundtripWithSerializer.cs
+++ src/K4os.Compression.LZ4.Streams.Test/RoundtripWithSerializer.cs
@@ -1,5 +1,8 @@
 using System.IO;
+using System.IO.Compression;
 using System.Runtime.Serialization;
+using System.Xml;
+
 using Xunit;
 
 namespace K4os.Compression.LZ4.Streams.Test
@@ -37,6 +40,49 @@ namespace K4os.Compression.LZ4.Streams.Test
 					Assert.Equal(obj.Test1, o.Test1);
 				}
 			}
+		}
+
+		[Fact]
+		public void Issue22_DataContractSerializerCausesInvalidDataException_Binary()
+		{
+			var obj = new TestClass { Test1 = "1" };
+			var serializer = new DataContractSerializer(typeof(TestClass));
+
+			byte[] bytes;
+			using (var ms = new MemoryStream())
+			{
+				using (var compressionStream = LZ4Stream.Encode(ms))
+				// using (var compressionStream = Gzip(ms))
+				{
+					using (var writer = XmlDictionaryWriter.CreateBinaryWriter(compressionStream))
+					{
+						serializer.WriteObject(writer, obj);
+					}
+				}
+
+				bytes = ms.ToArray();
+			}
+
+			using (var ms = new MemoryStream(bytes))
+			using (var decompressionStream = LZ4Stream.Decode(ms))
+			// using (var decompressionStream = UnGzip(ms))
+			using (var reader = XmlDictionaryReader.CreateBinaryReader(
+				decompressionStream, XmlDictionaryReaderQuotas.Max))
+			{
+				var o = serializer.ReadObject(reader) as TestClass;
+
+				Assert.Equal(obj.Test1, o.Test1);
+			}
+		}
+
+		private static GZipStream UnGzip(MemoryStream ms)
+		{
+			return new GZipStream(ms, CompressionMode.Decompress);
+		}
+
+		private static GZipStream Gzip(MemoryStream ms)
+		{
+			return new GZipStream(ms, CompressionMode.Compress);
 		}
 	}
 }
