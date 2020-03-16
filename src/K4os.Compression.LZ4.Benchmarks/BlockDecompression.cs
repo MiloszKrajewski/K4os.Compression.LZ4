480c80079fa3dc0f158b7b1e9c8238e9485b3df6
Milosz Krajewski <miloszkrajewski@o2.pl>
2020-03-16 09:02:22 +0000

    performance testing

---
+++ src/K4os.Compression.LZ4.Benchmarks/BlockDecompression.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Threading;
+using BenchmarkDotNet.Attributes;
+using K4os.Compression.LZ4.Internal;
+using TestHelpers;
+using LZ4PrevCodec = K4os.Compression.LZ4.vPrev.LZ4Codec;
+
+namespace K4os.Compression.LZ4.Benchmarks
+{
+	public class BlockDecompression
+	{
+		private byte[] _source;
+		private byte[] _target;
+
+		[GlobalSetup]
+		public void Setup()
+		{
+			var filename = Tools.FindFile(".corpus/xml");
+			var content = File.ReadAllBytes(filename);
+			var buffer = new byte[LZ4Codec.MaximumOutputSize(content.Length)];
+			var sourceLength = LZ4Codec.Encode(
+				content, 0, content.Length, 
+				buffer, 0, buffer.Length);
+			_source = new byte[sourceLength];
+			Array.Copy(buffer, _source, sourceLength);
+			_target = new byte[content.Length];
+		}
+
+		[Benchmark]
+		public void Previous64()
+		{
+			vPrev.LZ4Codec.Decode(
+				_source, 0, _source.Length,
+				_target, 0, _target.Length);
+		}
+		
+		// [Benchmark]
+		// public void Current32()
+		// {
+		// 	LZ4Codec.Enforce32 = true;
+		// 	LZ4Codec.Encode(
+		// 		_source, 0, _source.Length,
+		// 		_target, 0, _target.Length);
+		// 	LZ4Codec.Enforce32 = false;
+		// }
+		
+		[Benchmark]
+		public void Current64()
+		{
+			LZ4Codec.Decode(
+				_source, 0, _source.Length,
+				_target, 0, _target.Length);
+		}
+	}
+}
--- src/K4os.Compression.LZ4/Engine/LLxx.cs
+++ src/K4os.Compression.LZ4/Engine/LLxx.cs
@@ -12,9 +12,8 @@ using System.Runtime.CompilerServices;
 
 namespace K4os.Compression.LZ4.Engine
 {
-	internal unsafe class LLxx
+	internal static unsafe class LLxx
 	{
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int LZ4_decompress_safe(
 			byte* source, byte* target, int sourceLength, int targetLength) =>
 			LL.Algorithm32
@@ -23,7 +22,6 @@ namespace K4os.Compression.LZ4.Engine
 				: LL64.LZ4_decompress_safe(
 					source, target, sourceLength, targetLength);
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int LZ4_decompress_safe_continue(
 			LL.LZ4_streamDecode_t* context,
 			byte* source, byte* target, int sourceLength, int targetLength) =>
@@ -33,7 +31,6 @@ namespace K4os.Compression.LZ4.Engine
 				: LL64.LZ4_decompress_safe_continue(
 					context, source, target, sourceLength, targetLength);
 		
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int LZ4_compress_fast(
 			byte* source, byte* target, int sourceLength, int targetLength,
 			int acceleration) =>
@@ -43,7 +40,6 @@ namespace K4os.Compression.LZ4.Engine
 				: LL64.LZ4_compress_fast(
 					source, target, sourceLength, targetLength, acceleration);
 
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static int LZ4_compress_fast_continue(
 			LL.LZ4_stream_t* context,
 			byte* source, byte* target, int sourceLength, int targetLength,
