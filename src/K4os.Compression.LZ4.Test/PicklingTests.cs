4fc4b0e3a407aa3dba7dd44a6839037423d97452
Martin Taillefer <mataille@microsoft.com>
2021-01-20 06:48:52 -0800

    Improve pickling API and perf.
    - Introduce UnpickledSize which returns the expected size of a pickled
    item once unpickled.
    
    - Introduce an Unpickle overload which takes a Span<byte> and one that
    takes an IBufferWriter for output. Both of these enable the caller to
    reuse buffers across multiple unpicklings, which increases perf and
    reduces pressure on the GC.
    
    - Introduce a Pickle overload which takes an IBufferWriter as output.
    Using this overload enables the caller to reuse buffers, and eliminates
    a full data copy by unpickling directly into the final destination
    buffer.
    
    - The code was rewritten to mostly be safe by leveraging Span<T>. There
    is only a teeny bit of unsafe code left in the pickling path.

--- src/K4os.Compression.LZ4.Test/PicklingTests.cs
+++ src/K4os.Compression.LZ4.Test/PicklingTests.cs
@@ -1,4 +1,7 @@
 using System;
+#if NETSTANDARD2_1
+using System.Buffers;
+#endif
 using K4os.Compression.LZ4.Internal;
 using TestHelpers;
 using Xunit;
@@ -30,6 +33,37 @@ namespace K4os.Compression.LZ4.Test
 			Tools.SameBytes(original, unpickled);
 		}
 
+#if NETSTANDARD2_1
+		[Theory]
+		[InlineData(0)]
+		[InlineData(10)]
+		[InlineData(32)]
+		[InlineData(1337)]
+		[InlineData(1337, LZ4Level.L09_HC)]
+		[InlineData(0x10000)]
+		[InlineData(0x172a5, LZ4Level.L00_FAST)]
+		[InlineData(0x172a5, LZ4Level.L09_HC)]
+		[InlineData(0x172a5, LZ4Level.L11_OPT)]
+		[InlineData(0x172a5, LZ4Level.L12_MAX)]
+		[InlineData(Mem.M4, LZ4Level.L12_MAX)]
+		public void PickleLoremWithBufferWriter(int length, LZ4Level level = LZ4Level.L00_FAST)
+		{
+			var original = new byte[length];
+			Lorem.Fill(original, 0, length);
+
+			var pickledWriter = new ArrayBufferWriter<byte>();
+			var unpickledWriter = new ArrayBufferWriter<byte>();
+
+			LZ4Pickler.Pickle(original, pickledWriter, level);
+			var pickled = pickledWriter.WrittenSpan;
+
+			LZ4Pickler.Unpickle(pickled, unpickledWriter);
+			var unpickled = unpickledWriter.WrittenSpan;
+
+			Tools.SameBytes(original, unpickled);
+		}
+#endif
+
 		[Theory]
 		[InlineData(1, 15)]
 		[InlineData(2, 1024)]
@@ -47,6 +81,32 @@ namespace K4os.Compression.LZ4.Test
 
 			Tools.SameBytes(original, unpickled);
 		}
+
+#if NETSTANDARD2_1
+		[Theory]
+		[InlineData(1, 15)]
+		[InlineData(2, 1024)]
+		[InlineData(3, 1337, LZ4Level.L09_HC)]
+		[InlineData(3, 1337, LZ4Level.L12_MAX)]
+		[InlineData(4, Mem.K64, LZ4Level.L12_MAX)]
+		[InlineData(5, Mem.M4, LZ4Level.L12_MAX)]
+		public void PickleEntropyWithBufferWriter(int seed, int length, LZ4Level level = LZ4Level.L00_FAST)
+		{
+			var original = new byte[length];
+			new Random(seed).NextBytes(original);
+
+			var pickledWriter = new ArrayBufferWriter<byte>();
+			var unpickledWriter = new ArrayBufferWriter<byte>();
+
+			LZ4Pickler.Pickle(original, pickledWriter, level);
+			var pickled = pickledWriter.WrittenSpan;
+
+			LZ4Pickler.Unpickle(pickled, unpickledWriter);
+			var unpickled = unpickledWriter.WrittenSpan;
+
+			Tools.SameBytes(original, unpickled);
+		}
+#endif
 
 		[Theory]
 		[InlineData(0, 0)]
