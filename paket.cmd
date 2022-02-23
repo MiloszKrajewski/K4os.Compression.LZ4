c021e68b5690be9f197b788245e1569cd8ebd3ed
Milosz Krajewski <miloszkrajewski@o2.pl>
2022-02-23 09:48:15 +0000

    paket/fake as dotnet tools

--- paket.cmd
+++ paket.cmd
@@ -1,8 +1,5 @@
 @echo off
-setlocal
 pushd %~dp0
-set paket=.paket\paket.exe
-if not exist %paket% (dotnet tool install paket --tool-path %paket%\..)
-%paket% %*
+dotnet tool restore
+dotnet paket %*
 popd
-endlocal
