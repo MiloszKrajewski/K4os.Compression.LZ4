e5c16312c5057de807c149c38ef3efb7f78ccbaf
Milosz Krajewski <miloszkrajewski@o2.pl>
2022-11-06 21:42:54 +0000

    test hosts for issue #72

---
+++ assets/issue72/core60/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Buffers;
+using K4os.Compression.LZ4.Streams;
+
+var bytes = new byte[65536];
+var chunk = new byte[256];
+var random = new Random(0);
+random.NextBytes(chunk);
+for (var i = 0; i < bytes.Length; i += chunk.Length)
+	Buffer.BlockCopy(chunk, 0, bytes, i, chunk.Length);
+var encoded = LZ4Frame.Encode(bytes.AsSpan(), new ArrayBufferWriter<byte>());
+var ratio = (double)encoded.WrittenMemory.Length / bytes.Length;
+
+if (ratio > 1)
+	throw new Exception("Compression failed");
+
+Console.WriteLine("Compression ratio: {0:P2}", ratio);
---
+++ assets/issue72/net48/BufferWriter.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Buffers;
+
+// ReSharper disable once CheckNamespace
+namespace System.Buffers;
+
+public class BufferWriter: IBufferWriter<byte>
+{
+	private const int BLOCK_SIZE = 1024;
+
+	public static BufferWriter New() => new();
+	public static BufferWriter New(int size) => new(size);
+
+	private byte[] _buffer;
+	private int _position;
+
+	public BufferWriter() => _buffer = Array.Empty<byte>();
+	public BufferWriter(int size) => _buffer = Reallocate(RoundUp(size));
+
+	public ReadOnlySpan<byte> WrittenSpan => WrittenMemory.Span;
+	public ReadOnlyMemory<byte> WrittenMemory => _buffer.AsMemory(0, _position);
+
+	public void Advance(int count) => _position += count;
+
+	public Memory<byte> GetMemory(int sizeHint = 0) =>
+		Reallocate(RoundUp(_position + sizeHint)).AsMemory(_position);
+
+	public Span<byte> GetSpan(int sizeHint = 0) => GetMemory(sizeHint).Span;
+
+	private static int RoundUp(int value)
+	{
+		var size = BLOCK_SIZE;
+		while (size < value) size += size >> 1;
+		return size;
+	}
+
+	private byte[] Reallocate(int size)
+	{
+		if (_buffer is null || _buffer.Length < size)
+			Array.Resize(ref _buffer, size);
+		return _buffer;
+	}
+}
